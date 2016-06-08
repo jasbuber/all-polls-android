9c223843b4fd5aab12646f9b6c854a8102bd798c
Tomasz Jasianek <jasbuber@gmail.com>
2016-06-08 12:45:13 -0400

    Changes to lists styles

--- app/src/main/java/com/jasbuber/allpolls/PollsListAdapter.java
+++ app/src/main/java/com/jasbuber/allpolls/PollsListAdapter.java
@@ -32,7 +32,6 @@ public class PollsListAdapter extends RecyclerView.Adapter<PollsListAdapter.View
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.poll = polls.get(position);
         holder.mIdView.setText(polls.get(position).getTopic());
-        holder.mContentView.setText(polls.get(position).getTopic());
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,19 +51,17 @@ public class PollsListAdapter extends RecyclerView.Adapter<PollsListAdapter.View
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final TextView mIdView;
-        public final TextView mContentView;
         public Poll poll;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
             mIdView = (TextView) view.findViewById(R.id.id);
-            mContentView = (TextView) view.findViewById(R.id.content);
         }
 
         @Override
         public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            return super.toString() + " '" + mIdView.getText() + "'";
         }
     }
 }
