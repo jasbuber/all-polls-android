97301f1b15afdc4f0ad2dfc848129f4060f67936
Tomasz Jasianek <jasbuber@gmail.com>
2016-07-04 16:16:04 -0400

    Additional colors for charts

--- app/src/main/java/com/jasbuber/allpolls/services/ChartDisplayService.java
+++ app/src/main/java/com/jasbuber/allpolls/services/ChartDisplayService.java
@@ -21,6 +21,13 @@ import java.util.Map;
  */
 public class ChartDisplayService {
 
+    public static final int[] CHART_COLORS = {
+            Color.rgb(64, 89, 128), Color.rgb(149, 165, 124), Color.rgb(217, 184, 162),
+            Color.rgb(191, 134, 134), Color.rgb(179, 48, 80), Color.rgb(159, 154, 164),
+            Color.rgb(214, 249, 221), Color.rgb(248, 255, 244), Color.rgb(200, 197, 135),
+            Color.rgb(130, 212, 187)
+    };
+
     public PieData generatePieData(Map<String, Double> data) {
 
         List<String> labels = new ArrayList<>();
@@ -45,7 +52,7 @@ public class ChartDisplayService {
                 return String.format(Locale.US, "%.2f", value);
             }
         });
-        ds1.setColors(ColorTemplate.PASTEL_COLORS);
+        ds1.setColors(CHART_COLORS);
         ds1.setSliceSpace(2f);
         ds1.setValueTextColor(Color.WHITE);
         ds1.setValueTextSize(12f);
@@ -53,7 +60,7 @@ public class ChartDisplayService {
         return new PieData(labels, ds1);
     }
 
-    public void initializeChart(PieChart chart){
+    public void initializeChart(PieChart chart) {
         chart.setDrawHoleEnabled(false);
         chart.getLegend().setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
         chart.getLegend().setOrientation(Legend.LegendOrientation.VERTICAL);
